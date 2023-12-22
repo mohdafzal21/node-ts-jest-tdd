622abc1b395f77bab4c3d64362f3ca3301cc957e
mohdafzal47 <afzal@airinnovators.com>
2023-12-22 14:43:26 +0300

    init

---
+++ src/day1/__tests__/index.ts
@@ -0,0 +1,15 @@
+const { add } = require('../index');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+});
+
+test('adds 0 + 0 to equal 0', () => {
+    expect(add(0, 0)).toBe(0);
+});
+
+test('adds -1 + -1 to equal -2', () => {
+    expect(add(-1, -1)).toBe(-2);
+});
+
+// You can add more test cases as needed
